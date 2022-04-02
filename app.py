c05eab2b61dd58ea2ded545b2026e97f2b1ea4c3
Yuki <enginner@risaton.net>
2022-04-02 12:56:38 +0900

    commit

--- app.py
+++ app.py
@@ -7,16 +7,18 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-
 port = int(os.environ.get('PORT', 5000))
+
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @app.route('/test', methods=['POST'])
 def test():
     return "POST sent."
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
